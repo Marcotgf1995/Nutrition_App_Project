d76c7493ac7e3521fd362280e359d5ce3162ce5f
Marcotgf1995 <96400728+Marcotgf1995@users.noreply.github.com>
2023-06-09 20:25:43 -0400

    Initial Commit

---
+++ Nutrition_App/Nutrition_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nutrition_AppApp.swift
+//  Nutrition_App
+//
+//  Created by Marco Silva on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Nutrition_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
